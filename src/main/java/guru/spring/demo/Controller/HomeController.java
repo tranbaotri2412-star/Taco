21c213d4bcd6ce6c86a7163a811bbeeb750e7425
baotrihoccode <tranbaotri2412@gmail.com>
2026-07-14 10:52:30 +0700

    Validation errors

---
+++ src/main/java/guru/spring/demo/Controller/HomeController.java
@@ -0,0 +1,12 @@
+package guru.spring.demo.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+}
